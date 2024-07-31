1c6769fbc5408b6d5a92861e61cb1773fe88c5e1
KhinSuSuChit <44566464+KhinSuSuChit@users.noreply.github.com>
2024-07-31 23:13:57 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const categoriesInput = document.querySelector("#categories");
 
 const noteContainer = document.querySelector(".note-container");
 
+const modal = document.querySelector("#modal");
+const modalBtn = document.querySelector(".modal-btn");
+
 const categories = [
     {id:1, name:"Music"},
     {id:2, name:"Coding"},
@@ -40,13 +43,12 @@ searchInput.addEventListener("keyup",()=>{
         return keywordsToSearch.toLowerCase().includes(searchValue);
     });
 
-    noteContainer.innerHTML = "";
-
-    //add Notes to UI
-    filteredNotes.forEach((note)=>{
-        addNoteToUIList(note);
-    });
+    addFilteredNotesToUI(filteredNotes);
 });
+
+modalBtn.addEventListener("click", ()=> {
+    modal.close();
+})
 
 toggleBtn.addEventListener("click",()=>{
     if(theme === "light"){
@@ -59,8 +61,59 @@ toggleBtn.addEventListener("click",()=>{
     localStorage.setItem("theme", theme);
 });
 
+categoriesContainer.addEventListener("click", (event) => {
+    const currentItem = event.target.closest(".category-item");
+    const categoryId = currentItem.querySelector(".hidden-id").textContent;
+
+    let notes = getNotes();
+    let categorizedNotes = notes.filter((note) => {
+        return parseInt(note.categories) === parseInt(categoryId);
+    });
+
+    addFilteredNotesToUI(categorizedNotes);
+});
+
+noteContainer.addEventListener("click", (event) => {
+    const currentNote = event.target.closest(".note");
+
+    if(event.target.classList.contains('note-delete')){
+        const id = currentNote.querySelector("span").textContent;
+        if(confirm("Are you sure?")){
+            //remove from database
+            removeNote(id);
+            //remove from UI
+            currentNote.remove();
+            alert("Note deleted!")
+        }
+    }
+
+    if(event.target.classList.contains('note-view')){
+        
+        const noteTitle = currentNote.querySelector(".note-title").textContent;
+        const noteBody = currentNote.querySelector(".note-body").textContent;
+
+        openNoteModal(noteTitle, noteBody);
+    }
+
+});
+
+function openNoteModal(noteTitle, noteBody){
+    const modalTitle = document.querySelector(".modal-title");
+    const modalBody = document.querySelector(".modal-body");
+    modalTitle.textContent = noteTitle;
+    modalBody.textContent = noteBody;
+    modal.showModal()
+}
+
+function removeNote(id){
+    let notes = getNotes();
+    notes = notes.filter((note) => parseInt(note.id) !== parseInt(id))
+    localStorage.setItem('keep-notes', JSON.stringify(notes));
+}
+
 function loadTheme(){
     body.setAttribute("color-scheme",theme);
+    toggleBtn.innerHTML = theme==='light' ? '<i class="fa-solid fa-lightbulb"></i>' : '<i class="fa-solid fa-moon"></i>';
 }
 
 function showAllNotes(){
@@ -134,22 +187,28 @@ function saveToLocalStorage(item){
     localStorage.setItem('keep-notes', JSON.stringify(oldNotes));
 }
 
-/* 
-
-*/
+function addFilteredNotesToUI(filteredNotes){
+    noteContainer.innerHTML = "";
+    //add Notes to UI
+    filteredNotes.forEach((note)=>{
+        addNoteToUIList(note);
+    });
+}
 
 function addNoteToUIList(note){
     const article = document.createElement("article");
     article.classList.add("note");
     article.innerHTML = `
     <article>
+        <span hidden>${note.id}</span>
         <h2 class="note-title">${note.title}</h2>
         <p class="note-body">${note.note}</p>
         <div class="note-btns">
-            <button class="note-btn">View</button>
-            <button class="note-btn">Delete</button>
+            <button class="note-btn note-view">View</button>
+            <button class="note-btn note-delete">Delete</button>
         </div>
     </article>
     `;
     noteContainer.appendChild(article);
 }
+
